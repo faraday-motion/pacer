a1b900d2a31892dfd9e8e5b1da5e114698d5e6f1
Constantin Colac <constantin.colac@gmail.com>
2017-10-08 14:00:16 +0200

    Dead man's switch.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,37 @@
 #include <Arduino.h>
 #include "FMV.h"
+#include "components/MotorController/MotorController.h"
 #include "components/Console/Console.h"
 
+#define DEAD_MAN_SWITCH 2
+
 FMV fmv;
+bool DEAD_MAN_SWITCH_ENABLED = true;
+
+// Redundant Direct Connection to VESC
+MotorController mc;
+
+void handleDeadManSwitchInterrupt() {
+  DEAD_MAN_SWITCH_ENABLED = digitalRead(DEAD_MAN_SWITCH);
+}
 
 void setup() {
   Serial.begin(115200);
-  fmv.setup();;
+  pinMode(DEAD_MAN_SWITCH, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(DEAD_MAN_SWITCH), handleDeadManSwitchInterrupt, CHANGE);
+  mc.setup();
+  fmv.setup();
 
 }
 void loop() {
-  fmv.loop();
+  if (!DEAD_MAN_SWITCH_ENABLED) {
+    fmv.loop();
+  } else {
+    // Setting neutral current.
+    mc.set_current_brake(0);
+
+    Serial.println("::::::::PANIC:::::::::");
+    Serial.println("DEAD_MAN_SWITCH_ENABLED == TRUE");
+  }
   yield();
 }

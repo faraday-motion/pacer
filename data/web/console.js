c17081eb5ef64a8667348fcc4b3b591fbd92a3c1
Sune Pedersen <sune@faradaymotion.com>
2018-02-26 09:19:12 +0100

    Changed design and optimized for high performance log output

--- data/web/console.js
+++ data/web/console.js
@@ -1,4 +1,4 @@
-// Console function. 
+// Console function.
 
 var requestCommand; // holds the previously requested command.
 
@@ -16,19 +16,40 @@ function sendRequest()
   socket.send("{\"command\":" + JSON.parse(requestCommand) + ",\"value\":" + JSON.parse(payload) + "}");
 }
 
-function printResponse(response) {
+var iMaxRows = 250;
+var rows = [];
+var timerContent = null;
+var isPaused = false;
 
+function printResponse(response) {
   // Handle char and char array responses.
   if (requestCommand == 8008 || requestCommand == 8011) {
     response = parseCtrlId(response);
   }
-  document.getElementById("response").innerHTML = JSON.parse(JSON.stringify(response, undefined, 2)) + "<br>" + document.getElementById("response").innerHTML;
+  while (rows.length > iMaxRows)
+    rows.shift();
+  if (isPaused == false)
+    rows.push(JSON.parse(JSON.stringify(response, undefined, 2)) + "<br>");
+  if (timerContent == null)
+    timerContent = setTimeout(setContent, 250);
 }
 
+function pause() {
+  isPaused = !isPaused;
+}
+
+function setContent() {
+  var content = "";
+  for (i = 0; i < rows.length; i++) {
+    content = rows[i] + content;
+  }
+  document.getElementById("response").innerHTML = content;
+  timerContent = setTimeout(setContent, 100);
+}
 
 function parseCtrlId(data)
 {
-  let ctrls = []; 
+  let ctrls = [];
   var controllers = data.split('|');
   if (controllers.length) {
     for (a = 0; a < controllers.length; a++ ) {
